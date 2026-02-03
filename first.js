b074a2407c41c42c32b62c32a422498a2d4c625b
Akanksha Maurya <akankshamaurya91627@gmail.com>
2026-02-03 23:37:24 +0530

    document

---
+++ first.js
@@ -0,0 +1,20 @@
+// // console.log("hello");
+// // console.log("AKANKSHA MAURYA");
+// let name="AKANKSHA MAURYA";
+// console.log(name);
+// a=null;
+// isFollow=true;
+// console.log(isFollow);
+// let b=BigInt("123");
+// console.log(b);
+// let x= BigInt("123");
+// let y= Symbol("hello");
+const student={
+fullName:"Akanksha Maurya",
+age:21,
+cgpa:9.5,
+isGraduated:true
+};
+student["age"] = student["age"] + 1;
+console.log(student.age);
+console.log(student.cgpa);
---
+++ productPen.js
@@ -0,0 +1,14 @@
+// const productPen={
+// title:"Ball Pen",
+// rating:5,
+// offer:5,
+// price:270
+// };
+// console.log(productPen);
+const profile={
+name:"Akanksha Maurya",
+followers:22,
+following:25,
+isFollow:true
+};
+console.log(profile);
